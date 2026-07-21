eff4e21a6791117877d71b3012e1e92428b113bf
mustafa <mohebimustafa12@gmail.com>
2026-07-21 19:02:13 +0100

    created a new test method that verifies the number of options in a dropdown

--- tests/dropDown.spec.ts
+++ tests/dropDown.spec.ts
@@ -6,5 +6,15 @@ test("Verify DropDown", async({page})=>{
 
     page.goto("https://testautomationpractice.blogspot.com/");
 
-    
+    await page.locator("#country").selectOption('Canada');
+
+//Check the number of options in the dropdown(count):
+    //this snippet below just prints the number of options in the dropdown
+    const countryCount:number= await page.locator("#country>option").count();
+    console.log("number of country in this dropdown:", countryCount)
+
+    //But this snippet verfies the number of options in the dropdown with the toHaveCount() method
+    const countryCount2:Locator= page.locator("#country>option");
+    await expect(countryCount2).toHaveCount(10);
+
 });
