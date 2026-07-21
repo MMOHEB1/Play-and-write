62a1ec65a243705498bfc77655fd5a63aa51f98a
mustafa <mohebimustafa12@gmail.com>
2026-07-21 18:40:24 +0100

    added new test for dropdowns

---
+++ tests/dropDown.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect,Locator } from "@playwright/test";
+
+
+
+test("Verify DropDown", async({page})=>{
+
+    page.goto("https://testautomationpractice.blogspot.com/");
+
+    
+});
